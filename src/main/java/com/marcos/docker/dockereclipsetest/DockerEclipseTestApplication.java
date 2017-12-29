067bc35733f7487cdd6563177d1b58ddf8d02563
Markos Test <lounge_act_1991@hotmail.com>
2017-12-29 13:22:58 -0600

    Initial commit

---
+++ src/main/java/com/marcos/docker/dockereclipsetest/DockerEclipseTestApplication.java
@@ -0,0 +1,12 @@
+package com.marcos.docker.dockereclipsetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerEclipseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerEclipseTestApplication.class, args);
+	}
+}
